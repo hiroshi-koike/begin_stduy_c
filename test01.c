0f293e789f7fac81e0b92d5133e09daf8bd35bc8
Hiroshi Koike <74845251+hiroshi-koike@users.noreply.github.com>
2020-12-24 18:26:52 +0900

    Learning pointer
    I'm study Pointer

--- test01.c
+++ test01.c
@@ -4,10 +4,17 @@
 
 int main()
 {
-    int a = 0;
+    int int_a = 123456789;
+    int int_b = 0;
+    int * int_p = 0;
     char moji[20];
 
     memset(&moji[0],0x00,sizeof(moji));
     strcpy(&moji[0], "i'm a programmer");
-    printf("%s\n", moji);
+
+    int_p = &int_a;
+    int_b = *int_p;
+    printf("%d\n", int_a);
+    printf("%d\n", int_b);
+    printf("%ld\n", sizeof(int));
 }
